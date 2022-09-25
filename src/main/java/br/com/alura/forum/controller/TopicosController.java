2944b4e10d85ae686ec8fbcc5ee486a358d1cf4d
Wallaks Cardoso da Silva <42080153+Wallaks@users.noreply.github.com>
2022-09-25 12:04:14 -0300

    Criando a requisição para atualizar ultilizando mais um form para dizer o que eu cadastro e o que posso alterar.

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 
 import br.com.alura.forum.controller.dto.DetalhesTopicoDto;
+import br.com.alura.forum.controller.form.AtualizacaoTopicoForm;
 import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
@@ -13,6 +14,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +22,8 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.modelo.Topico;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.transaction.Transactional;
 
 @RestController
 @RequestMapping("/topicos")
@@ -52,8 +56,16 @@ public class TopicosController {
 
 	@GetMapping("/{id}")
 	public DetalhesTopicoDto detalhar(@PathVariable Long id) {
-		Topico topico = topicoRepository.getOne(id);
+		Topico topico = topicoRepository.getReferenceById(id);
 		return new DetalhesTopicoDto(topico);
+
+	}
+
+	@PutMapping("/{id}")
+	@Transactional
+	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Validated AtualizacaoTopicoForm form) {
+	Topico topico = form.atualizar(id, topicoRepository);
+		return ResponseEntity.ok(new TopicoDto(topico));
 
 	}
 
